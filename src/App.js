cd7a723acce14ceef86f6417777f87b056d31fa9
Fernando <fernando.santos@bandtec.com.br>
2022-04-11 12:12:44 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InstitucionalPage from "./pages/InstitucionalPage";
 import DashBoardPage from "./pages/DashBoardPage";
 import SignPage from "./pages/SignPage";
@@ -7,22 +7,24 @@ import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<InstitucionalPage />} />
-        <Route path="dashboard" element={<DashBoardPage />} />
-        <Route path="cadastro" element={<SignPage />} />
-        <Route path="error">
-          <Route
-            path={sessionStorage.getItem("status")}
-            element={<ErrorPage status={sessionStorage.getItem("status")} />}
-          />
-          <Route path={"404"} element={<ErrorPage status={"404"} />} />
-          <Route path={"500"} element={<ErrorPage status={"500"} />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<InstitucionalPage />} />
+          <Route path="dashboard" element={<DashBoardPage />} />
+          <Route path="cadastro" element={<SignPage />} />
+          <Route path="error">
+            <Route
+              path={sessionStorage.getItem("status")}
+              element={<ErrorPage status={sessionStorage.getItem("status")} />}
+            />
+            <Route path={"404"} element={<ErrorPage status={"404"} />} />
+            <Route path={"500"} element={<ErrorPage status={"500"} />} />
+          </Route>
+          <Route path="*" element={<ErrorPage status={"404"} />} />
         </Route>
-        <Route path="*" element={<ErrorPage status={"404"} />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import DocumentTitle from "react-document-title";
-import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
     <DocumentTitle title="Sweet Stock" />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
