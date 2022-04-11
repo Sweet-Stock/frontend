67d6bdef233f5de57fc1ba0a35c253d17fd70b54
Fernando <fernando.santos@bandtec.com.br>
2022-04-11 12:09:14 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import DocumentTitle from "react-document-title";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     <DocumentTitle title="Sweet Stock" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
