38feb917ca5293e3f55578a145c1332af3821e04
Fernando <fernando.santos@bandtec.com.br>
2022-04-17 00:29:52 -0300

    a

--- src/pages/SignPage.jsx
+++ src/pages/SignPage.jsx
@@ -11,68 +11,6 @@ import api from "../services/api";
 import "./SignPage.css";
 
 export default () => {
-  useEffect(() => {
-    api
-      .get("http://localhost:8080/v1/sweet-stock/products")
-      .then((response) => {
-        console.log(response.data);
-        // console.log(response.status);
-        // sessionStorage.setItem("data", JSON.stringify(response.data));
-        // sessionStorage.setItem("status", response.status);
-        // login(sessionStorage.getItem("status"));
-      })
-      .catch((err) => {
-        console.error(err);
-        // sessionStorage.setItem(
-        //   "status",
-        //   JSON.stringify(err.response.status)
-        // );
-        // console.log(err.response.status);
-        // login(sessionStorage.getItem("status"));
-      });
-  }, []);
-  useEffect(() => {
-    api
-      .post(
-        "http://localhost:8080/v1/sweet-stock/companies",
-        {
-          name: "inputName",
-          fantasyName: "inputFantasyName",
-          telephoneNumber: "inputPhone",
-          peopleType: "FISICA",
-          ceo: "inputCEO",
-          cpf: "inputCPF",
-          cnpj: null,
-          addressRequest: {
-            street: "inputStreet",
-            number: "inputNumber",
-            complement: "inputComplement",
-            city: "inputCity",
-            state: "inputState",
-            neighborhood: "inputNeighbor",
-          },
-          email: "inpufdsssdaweadsassaatEmail",
-          password: "inpufdsfdsfds",
-        }
-      )
-      .then((response) => {
-        console.log(response.data);
-        // console.log(response.status);
-        // sessionStorage.setItem("data", JSON.stringify(response.data));
-        // sessionStorage.setItem("status", response.status);
-        // login(sessionStorage.getItem("status"));
-      })
-      .catch((err) => {
-        console.error(err);
-        // sessionStorage.setItem(
-        //   "status",
-        //   JSON.stringify(err.response.status)
-        // );
-        // console.log(err.response.status);
-        // login(sessionStorage.getItem("status"));
-      });
-  }, []);
-
   const [signProgress, setSignProgress] = React.useState(0);
   const [person, setPerson] = React.useState("FISICA");
   const [password, setPassword] = React.useState(true);
