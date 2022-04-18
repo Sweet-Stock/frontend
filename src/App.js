1f3f54cc7640bd5757f5adbf5e7c003159ac5bb5
MarceloCostaP <marcelo.pereira@sptech.school>
2022-04-18 20:49:52 -0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import InstitucionalPage from "./pages/InstitucionalPage";
-import DashBoardPage from "./pages/DashBoardPage";
-import SignPage from "./pages/SignPage";
-import LoginPage from "./pages/LoginPage";
-import ErrorPage from "./pages/ErrorPage";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<InstitucionalPage />} />
-          <Route path="dashboard" element={<DashBoardPage />} />
-          <Route path="cadastro" element={<SignPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="error">
-            <Route
-              path={sessionStorage.getItem("status")}
-              element={<ErrorPage status={sessionStorage.getItem("status")} />}
-            />
-            <Route path={"404"} element={<ErrorPage status={"404"} />} />
-            <Route path={"500"} element={<ErrorPage status={"500"} />} />
-          </Route>
-          <Route path="*" element={<ErrorPage status={"404"} />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes/>
   );
 }
 
---
+++ src/Routes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InstitucionalPage from "./pages/InstitucionalPage";
+import DashBoardPage from "./pages/DashBoardPage";
+import SignPage from "./pages/SignPage";
+import LoginPage from "./pages/LoginPage";
+import ErrorPage from "./pages/ErrorPage";
+
+export default () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<InstitucionalPage />} />
+                    <Route path="dashboard" element={<DashBoardPage />} />
+                    <Route path="cadastro" element={<SignPage />} />
+                    <Route path="login" element={<LoginPage />} />
+                    <Route path="error">
+                        <Route
+                            path={sessionStorage.getItem("status")}
+                            element={<ErrorPage status={sessionStorage.getItem("status")} />}
+                        />
+                        <Route path={"404"} element={<ErrorPage status={"404"} />} />
+                        <Route path={"500"} element={<ErrorPage status={"500"} />} />
+                    </Route>
+                    <Route path="*" element={<ErrorPage status={"404"} />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
