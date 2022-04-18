8a2279288b02ebb6ab8750796f84e09811d2196b
Fernando <fernando.santos@bandtec.com.br>
2022-04-18 00:24:43 -0300

    aSDSDFATGEFR5VDC ZXGAGTERHFDAETGR5YFCD

--- src/pages/DashBoardPage.jsx
+++ src/pages/DashBoardPage.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import SideBar from "./DashboardPage/SideBar";
 
 export default () => {
-  switch (1) {
-    case 1:
-      return(<div></div>)
-  
-    default:
-      break;
-  }
   return (
     <>
       <SideBar name="Rafael"/>
