ac150e31732320619bf85d66459364b07cfd8a3d
Fernando <fernando.santos@bandtec.com.br>
2022-05-23 23:18:45 -0300

    a

--- src/pages/DashboardPage/dashboard_ingredients/DashIngredients.jsx
+++ src/pages/DashboardPage/dashboard_ingredients/DashIngredients.jsx
@@ -1,22 +1,20 @@
-import PlusBtn from "../../images/plusbtn.svg";
-
-import { DashboardIngredients } from "../../components/Ingredient_modal_card/IngredientsCardDash";
-import { useState, useEffect } from "react";
-import api from "../../../services/api";
-import DashBoardIngredientsPage from "./dashboard_ingredients_cards_page/DashBoardIngredientsPage";
-import DashboardIngredientsForm from "./dashboard_ingredients_form_page/DashboardIngredientsForm";
+import PlusBtn from '../../images/plusbtn.svg'
+import { DashboardIngredients } from '../../components/Ingredient_modal_card/IngredientsCardDash'
+import { useState, useEffect } from 'react'
+import api from '../../../services/api'
+import DashBoardIngredientsPage from './dashboard_ingredients_cards_page/DashBoardIngredientsPage'
+import DashboardIngredientsForm from './dashboard_ingredients_form_page/DashboardIngredientsForm'
 
 export default ({ grow }) => {
-  const [data, setData] = useState([]);
-  const [page, setPage] = useState(true);
+  const [page, setPage] = useState(true)
 
   switch (page) {
     case true:
-      return <DashBoardIngredientsPage setPage={setPage} grow={grow} />;
+      return <DashBoardIngredientsPage setPage={setPage} grow={grow} />
     case false:
-      return <DashboardIngredientsForm setPage={setPage} grow={grow} />;
+      return <DashboardIngredientsForm setPage={setPage} grow={grow} />
 
     default:
-      break;
+      break
   }
-};
+}
--- src/pages/DashboardPage/dashboard_ingredients/dashboard_ingredients_form_page/DashboardIngredientsForm.jsx
+++ src/pages/DashboardPage/dashboard_ingredients/dashboard_ingredients_form_page/DashboardIngredientsForm.jsx
@@ -1,113 +1,123 @@
-import { useState } from "react";
-import api from "../../../../services/api";
-import Button from "../../../components/button/Button";
-import Input from "../../../components/input/Input";
-import InputFile from "../../../components/input_file/InputFile";
-import ProgressBar from "../../../components/progress_bar/ProgressBar";
-import Select from "../../../components/select/Select";
+import { useState } from 'react'
+import api from '../../../../services/api'
+import Button from '../../../components/button/Button'
+import Input from '../../../components/input/Input'
+import InputFile from '../../../components/input_file/InputFile'
+import ProgressBar from '../../../components/progress_bar/ProgressBar'
+import Select from '../../../components/select/Select'
 
 export default ({ grow, setPage }) => {
-  const [isRequired, setIsRequired] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [isRequired, setIsRequired] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(false)
 
-  const [inputName, setInputName] = useState(null);
-  const [inputBrand, setInputBrand] = useState(null);
-  const [inputProvider, setInputProvider] = useState(null);
-  const [inputBuyDate, setInputBuyDate] = useState(null);
-  const [inputValDate, setInputValDate] = useState(null);
-  const [inputStorage, setInputStorage] = useState(null);
-  const [inputMetric, setInputMetric] = useState(null);
-  const [inputValue, setInputValue] = useState(null);
-  const [inputPicture, setInputPicture] = useState(null);
+  const [inputName, setInputName] = useState(null)
+  const [inputBrand, setInputBrand] = useState(null)
+  const [inputProvider, setInputProvider] = useState(null)
+  const [inputBuyDate, setInputBuyDate] = useState(null)
+  const [inputValDate, setInputValDate] = useState(null)
+  const [inputStorage, setInputStorage] = useState(null)
+  const [inputStorageType, setInputStorageType] = useState(null)
+  const [inputMetric, setInputMetric] = useState(null)
+  const [inputValue, setInputValue] = useState(null)
+  const [inputPicture, setInputPicture] = useState(null)
 
-  const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState(0)
 
-  const [providers, setProviders] = useState([]);
+  const [providers, setProviders] = useState([])
 
-  const isRefigerated = [
+  const isRefrigerated = [
     {
       name: true,
-      text: "Refrigerado",
+      text: 'Refrigerado'
     },
     {
       name: false,
-      text: "Não refrigerado",
-    },
-  ];
+      text: 'Não refrigerado'
+    }
+  ]
   const unitMetrics = [
     {
-      name: "LITRO",
-      text: "Litros (L)",
+      name: 'LITRO',
+      text: 'Litros (L)'
     },
     {
-      name: "MILILITRO",
-      text: "Mililitros (ML)",
+      name: 'MILILITRO',
+      text: 'Mililitros (ML)'
     },
     {
-      name: "QUILO",
-      text: "Quilos (KG)",
+      name: 'QUILO',
+      text: 'Quilos (KG)'
     },
     {
-      name: "MILIGRAMA",
-      text: "Miligramas (MG)",
+      name: 'MILIGRAMA',
+      text: 'Miligramas (MG)'
     },
     {
-      name: "UNIDADE",
-      text: "Unidades (UN)",
+      name: 'UNIDADE',
+      text: 'Unidades (UN)'
     },
     {
-      name: "GRAMA",
-      text: "Gramas (GM)",
-    },
-  ];
-
-  const config = {
-    headers: {
-      Authorization:
-        "Bearer " + JSON.parse(sessionStorage.getItem("data")).token,
-    },
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (progress !== 2) {
-      setProgress(progress + 1);
-      return;
+      name: 'GRAMA',
+      text: 'Gramas (GM)'
     }
+  ]
 
-    if (
-      inputName === null &&
-      inputBrand === null &&
-      inputBuyDate === null &&
-      inputMetric === null
-    ) {
-      setIsRequired(true);
-      setIsLoading(true);
-      setIsDisabled(true);
+  // const config = {
+  //   headers: {
+  //     Authorization:
+  //       "Bearer " + JSON.parse(sessionStorage.getItem("data")).token,
+  //   },
+  // };
 
-      await api.post("", config);
+  const handleSubmit = async e => {
+    e.preventDefault()
 
-      setIsLoading(false);
-      setIsDisabled(false);
+    switch (progress) {
+      case 0:
+        if (inputName == null || inputBrand == null || inputProvider == null) {
+          setIsRequired(true)
+        } else {
+          setIsRequired(false)
+          setProgress(progress + 1)
+        }
+        break
+      case 1:
+        if (inputBuyDate == null || inputValDate == null) {
+          setIsRequired(true)
+        } else {
+          setIsRequired(false)
+          setProgress(progress + 1)
+        }
+        break
+      case 2:
+        if (inputValue == null || inputStorage == null) {
+          setIsRequired(true)
+        } else {
+          setIsRequired(false)
+          setProgress(progress + 1)
+        }
+        break
+
+      default:
+        break
     }
-  };
+  }
 
-  const toBase64 = (file) =>
+  const toBase64 = file =>
     new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = (error) => reject(error);
-    });
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => resolve(reader.result)
+      reader.onerror = error => reject(error)
+    })
 
   return (
     <section
       className={
         grow % 2 === 0
-          ? "relative w-[100%] h-[100vh] pl-48 transition-all ease-in-out duration-500 flex flex-col items-center"
-          : "relative w-[100%] h-[100vh] pl-80 transition-all ease-in-out duration-500 flex flex-col items-center"
+          ? 'relative w-[100%] h-[100vh] pl-48 transition-all ease-in-out duration-500 flex flex-col items-center'
+          : 'relative w-[100%] h-[100vh] pl-80 transition-all ease-in-out duration-500 flex flex-col items-center'
       }
     >
       <div className="w-[100%] flex flex-row gap-2 items-center justify-between pt-20 pr-10">
@@ -128,28 +138,28 @@ export default ({ grow, setPage }) => {
             isNotVisible={progress !== 0}
             label="Nome"
             message="Por favor insira um nome"
-            onChange={(e) => setInputName(e.target.value)}
+            onChange={e => setInputName(e.target.value)}
             required={isRequired}
           />
           <Input
             isNotVisible={progress !== 0}
             label="Marca"
             message="Por favor insira uma marca"
-            onChange={(e) => setInputBrand(e.target.value)}
+            onChange={e => setInputBrand(e.target.value)}
             required={isRequired}
           />
           <Input
             isNotVisible={progress !== 0}
             label="Fornecedor"
             message="Por favor insira um fornecedor"
-            onChange={(e) => setInputProvider(e.target.value)}
+            onChange={e => setInputProvider(e.target.value)}
             required={isRequired}
           />
           <Input
-            isNotVisible={progress !== 0}
+            isNotVisible={progress !== 1}
             label="Data de compra"
             message="Por favor insira uma data"
-            onChange
+            onChange={e => setInputBuyDate(e.target.value)}
             required={isRequired}
             type="date"
           />
@@ -157,21 +167,21 @@ export default ({ grow, setPage }) => {
             isNotVisible={progress !== 1}
             label="Data de validade"
             message="Por favor insira uma data"
-            onChange
+            onChange={e => setInputValDate(e.target.value)}
             required={isRequired}
             type="date"
           />
           <Select
             isNotVisible={progress !== 1}
             label="Armazenamento"
-            onChange
+            onChange={e => setInputStorageType(e.target.value)}
             required={isRequired}
-            options={isRefigerated}
+            options={isRefrigerated}
           />
           <Select
             isNotVisible={progress !== 1}
             label="Unidade de medida"
-            onChange
+            onChange={e => setInputMetric(e.target.value)}
             required={isRequired}
             options={unitMetrics}
           />
@@ -180,16 +190,16 @@ export default ({ grow, setPage }) => {
             label="Quantidade em estoque"
             pattern="^[0-9]*$"
             message="Por favor insira apenas números"
-            onChange
+            onChange={e => setInputStorage(e.target.value)}
             required={isRequired}
             placeholder="99999"
           />
           <Input
             isNotVisible={progress !== 2}
             label="Valor"
-            pattern="^[0-9]*$"
+            pattern="^-?(?:0|[1-9][0-9]*)\.?[0-9]+$"
             message="Por favor insira apenas números"
-            onChange
+            onChange={e => setInputValue(e.target.value)}
             required={isRequired}
             placeholder="99,99"
           />
@@ -198,30 +208,30 @@ export default ({ grow, setPage }) => {
             label="Imagem"
             message="Por favor insira uma Imagem"
             onChange={async () => {
-              const file = document.querySelector("#input_file_id").files[0];
-              const fileInBase64 = await toBase64(file);
+              const file = document.querySelector('#input_file_id').files[0]
+              const fileInBase64 = await toBase64(file)
 
-              setInputPicture(fileInBase64);
+              fileInBase64.startsWith('data:image/') &&
+                setInputPicture(fileInBase64)
             }}
-            required={isRequired}
           />
 
           <div className="flex w-1/2 justify-between items-center">
             <Button
               isDisabled={isDisabled}
               onClick={() => {
-                progress === 0 ? setPage(true) : setProgress(progress - 1);
+                progress === 0 ? setPage(true) : setProgress(progress - 1)
               }}
               content="Voltar"
             />
             <Button
               isLoading={isLoading}
               type="submit"
-              content={progress === 2 ? "Cadastrar" : "Continuar"}
+              content={progress === 2 ? 'Cadastrar' : 'Continuar'}
             />
           </div>
         </form>
       </div>
     </section>
-  );
-};
+  )
+}
