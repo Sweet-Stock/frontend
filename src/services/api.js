3031e8ee1b7b389ddbdf4fd2a567f18939729143
Fernando Marques dos Santos <79321540+Fernandosantos1@users.noreply.github.com>
2022-10-18 21:27:46 -0300

    b
    b

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const env = process.env.REACT_APP_API_KEY
 
 const api = axios.create({
-  baseURL: env ?? 'http://54.225.132.238/v1/sweet-stock/'
+  baseURL: env ?? 'https://sweetstock-backend.servehttp.com/v1/sweet-stock/'
 })
 
 export default api
